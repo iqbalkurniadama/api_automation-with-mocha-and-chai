6d96c240e0800c3a3a876f22a2e921f31beda7ba
iqbalkurnia <iqbalkurnia091@gmail.com>
2023-07-30 15:36:31 +0700

    add report in testing api

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # api_automation-with-mocha-and-chai
 Tugas Sanbercode - API Automation
+
+## How to Run
+
+install depedencies
+```
+npm install
+```
+
+run program
+```
+mocha test/suite/allTest.js --reporter mochawesome
+```
+
+cek report
+1. open folder mochawesome
+2. click file html mochawesome
+3. finish
+
+### Thanks
--- test/suite/allTest.js
+++ test/suite/allTest.js
@@ -2,12 +2,12 @@ const request = require("supertest")("https://kasir-api.belajarqa.com");
 const expect = require("chai").expect;
 const { Token, detailProduct } = require("../../config");
 
-const login = require("./login");
-const addProduct = require("./addProduct");
-const getAllProduct = require("./getAllProduct");
-const getDetailProduct = require("./getDetailProduct");
-const updateProduct = require("./updateProduct");
-const deleteProduct = require("./deleteProduct");
+const login = require("../spec/login");
+const addProduct = require("../spec/addProduct");
+const getAllProduct = require("../spec/getAllProduct");
+const getDetailProduct = require("../spec/getDetailProduct");
+const updateProduct = require("../spec/updateProduct");
+const deleteProduct = require("../spec/deleteProduct");
 
 describe("All Test", () => {
   describe("Login", login);
